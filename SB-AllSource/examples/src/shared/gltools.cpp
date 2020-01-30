9cafe8f809d4f6409d45055d72c33105e75894a8
Bill Sixx <billsix@gmail.com>
2020-01-29 22:05:04 -0500

    updated through chapter 5 for macOS

--- SB-AllSource/examples/src/shared/gltools.cpp
+++ SB-AllSource/examples/src/shared/gltools.cpp
@@ -114,61 +114,6 @@ int gltIsWGLExtSupported(HDC hDC, const char *szExtension)
   #endif
 
 
-/////////////////////////////////////////////////////////////
-// Get a pointer to an OpenGL extension
-// Note on the Mac, this does a lot of work that could be saved
-// if you call this function repeatedly. Write your own function that
-// gets the bundle once, gets all the function pointers, then releases
-// the bundle.
-void *gltGetExtensionPointer(const char *szExtensionName)
-	{
-#ifdef WIN32
-    // Well, this one is simple. An OpenGL context must be
-    // current first. Returns NULL if extension not supported
-    return (void *)wglGetProcAddress(szExtensionName);
-#endif
-	
-#ifdef linux
-    // Pretty much ditto above
-    return (void *)glXGetProcAddress((GLubyte *)szExtensionName);
-#endif
-
-    
-#ifdef __APPLE__
-    // Mac is a bit more tricky.
-    // First we need the bundle
-    CFBundleRef openGL = 0;
-    SInt16      fwVersion = 0;
-    SInt32      fwDir = 0;
-    
-    if(FindFolder(kSystemDomain, kFrameworksFolderType, kDontCreateFolder, &fwVersion, &fwDir) != noErr)
-        return NULL;
-	
-    FSSpec fSpec;
-    FSRef  fRef;
-    if(FSMakeFSSpec(fwVersion, fwDir, "\pOpenGL.framework", &fSpec) != noErr)
-        return NULL;
-	
-    FSpMakeFSRef(&fSpec, &fRef);
-    CFURLRef url = CFURLCreateFromFSRef(kCFAllocatorDefault, &fRef);
-    if(!url)
-        return NULL;
-	
-    openGL = CFBundleCreate(kCFAllocatorDefault, url);
-    CFRelease(url);
-    
-    // Then load the function pointer from the bundle
-    CFStringRef string = CFStringCreateWithCString(kCFAllocatorDefault, szExtensionName, kCFStringEncodingMacRoman);
-    void *pFunc = CFBundleGetFunctionPointerForName(openGL, string);
-    
-    // Release the bundle and string
-    CFRelease(string);
-    CFRelease(openGL);
-    
-    // Return the function ponter
-    return pFunc;
-#endif    
-    }
 
 
 ///////////////////////////////////////////////////////////////////
@@ -480,14 +425,6 @@ GLbyte *gltLoadTGA(const char *szFileName, GLint *iWidth, GLint *iHeight, GLint 
     fread(&tgaHeader, 18/* sizeof(TGAHEADER)*/, 1, pFile);
     
     // Do byte swap for big vs little endian
-#ifdef __APPLE__
-    LITTLE_ENDIAN_WORD(&tgaHeader.colorMapStart);
-    LITTLE_ENDIAN_WORD(&tgaHeader.colorMapLength);
-    LITTLE_ENDIAN_WORD(&tgaHeader.xstart);
-    LITTLE_ENDIAN_WORD(&tgaHeader.ystart);
-    LITTLE_ENDIAN_WORD(&tgaHeader.width);
-    LITTLE_ENDIAN_WORD(&tgaHeader.height);
-#endif
 	
 	
     // Get width, height, and depth of texture
